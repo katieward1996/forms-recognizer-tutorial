aa5d76e44115df11913f5d6a5089c0bf2239bec1
katward <60496885+katward@users.noreply.github.com>
2020-06-18 09:49:03 +1000

    Add files via upload
    Full python script that trains and analyses a model. Saves output to local file.

---
+++ form-recogniser-train-analyse.py
@@ -0,0 +1,133 @@
+########### Python Form Recognizer Labeled Async Train #############
+
+import json, os
+import time
+from requests import get, post
+
+os.chdir(r"C:\<INSERT YOUR PATH HERE>")
+
+# Endpoint URL
+endpoint = r"<endpoint>"
+post_url = endpoint + r"/formrecognizer/v2.0-preview/custom/models"
+source = r"<SAS URL>"
+prefix = "" #Path to the folder in blob storage where your forms are located. If your forms are at the root of your container, leave this string empty.
+includeSubFolders = False
+useLabelFile = False
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '<subscription key>', 
+}
+
+body = 	{
+    "source": source,
+    "sourceFilter": {
+        "prefix": prefix,
+        "includeSubFolders": includeSubFolders
+    },
+    "useLabelFile": useLabelFile
+}
+
+try:
+    resp = post(url = post_url, json = body, headers = headers)
+    if resp.status_code != 201:
+        print("POST model failed (%s):\n%s" % (resp.status_code, json.dumps(resp.json())))
+        quit()
+    print("POST model succeeded:\n%s" % resp.headers)
+    get_url = resp.headers["location"]
+except Exception as e:
+    print("POST model failed:\n%s" % str(e))
+    quit()
+
+
+n_tries = 15
+n_try = 0
+wait_sec = 5
+max_wait_sec = 60
+while n_try < n_tries:
+    try:
+        resp = get(url = get_url, headers = headers)
+        resp_json = resp.json()
+        if resp.status_code != 200:
+            print("GET model failed (%s):\n%s" % (resp.status_code, json.dumps(resp_json)))
+            quit()
+        model_status = resp_json["modelInfo"]["status"]
+        if model_status == "ready":
+            print("Training succeeded:\n%s" % json.dumps(resp_json, indent=4, sort_keys=True))
+            modelID = resp_json["modelInfo"]["modelId"]
+            print("ModelID:\n%s" % modelID)
+            break
+        if model_status == "invalid":
+            print("Training failed. Model is invalid:\n%s" % json.dumps(resp_json))
+            quit()
+        # Training still running. Wait and retry.
+        time.sleep(wait_sec)
+        n_try += 1
+        wait_sec = min(2*wait_sec, max_wait_sec)     
+    except Exception as e:
+        msg = "GET model failed:\n%s" % str(e)
+        print(msg)
+        quit()
+
+
+########### Python Form Recognizer Async Analyze #############
+
+# Endpoint URL
+endpoint = r"<endpoint>"
+apim_key = "<subscription key>"
+post_url = endpoint + "/formrecognizer/v2.0-preview/custom/models/%s/analyze" % modelID
+source = r"C:\<insert file path to form you want to analyse>"
+params = {
+    "includeTextDetails": True
+}
+
+headers = {
+    # Request headers
+    'Content-Type': '<file type>', # Supported types: application/pdf, image/jpeg, image/png, image/tiff
+    'Ocp-Apim-Subscription-Key': apim_key,
+}
+with open(source, "rb") as f:
+    data_bytes = f.read()
+
+try:
+    resp = post(url = post_url, data = data_bytes, headers = headers, params = params)
+    if resp.status_code != 202:
+        print("POST analyze failed:\n%s" % json.dumps(resp.json()))
+        quit()
+    print("POST analyze succeeded:\n%s" % resp.headers)
+    get_url = resp.headers["operation-location"]
+except Exception as e:
+    print("POST analyze failed:\n%s" % str(e))
+    quit()
+
+
+ocrFileName = 'fr-results.json' 
+output_file = open(ocrFileName,'w')
+n_tries = 15
+n_try = 0
+wait_sec = 5
+max_wait_sec = 60
+while n_try < n_tries:
+    try:
+        resp = get(url = get_url, headers = {"Ocp-Apim-Subscription-Key": apim_key})
+        resp_json = resp.json()
+        if resp.status_code != 200:
+            print("GET analyze results failed:\n%s" % json.dumps(resp_json))
+            quit()
+        status = resp_json["status"]
+        if status == "succeeded":
+            print("Analysis succeeded:\n%s" % json.dumps(resp_json,indent=4, sort_keys=True))
+            json.dump(resp_json, output_file)
+            break
+        if status == "failed":
+            print("Analysis failed:\n%s" % json.dumps(resp_json))
+            quit()
+        # Analysis still running. Wait and retry.
+        time.sleep(wait_sec)
+        n_try += 1
+        wait_sec = min(2*wait_sec, max_wait_sec)     
+    except Exception as e:
+        msg = "GET analyze results failed:\n%s" % str(e)
+        print(msg)
+        quit()
